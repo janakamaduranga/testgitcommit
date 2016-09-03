211c5073cf0454534ef0ed99a9931fc84ceaa671
janaka <janaka.maduranga@gmail.com>
2016-09-03 18:02:01 +0800

    xxxx

--- Test/src/com/test/Test.java
+++ Test/src/com/test/Test.java
@@ -5,7 +5,7 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.println("testssss");
 		System.out.println("index");
-		System.out.println("commit");
+		System.out.println("commit--xxxxxxxxxx");
 		
 		System.out.println("ss");
 		
