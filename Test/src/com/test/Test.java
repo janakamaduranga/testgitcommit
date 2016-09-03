35ce54a0af4b5ca3361b9da568b7dc6f4f77f64c
janaka <janaka.maduranga@gmail.com>
2016-09-03 18:07:32 +0800

    ddddd

--- Test/src/com/test/Test.java
+++ Test/src/com/test/Test.java
@@ -5,7 +5,7 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.println("testssss");
 		System.out.println("index");
-		System.out.println("commit--changed");
+		System.out.println("commit--changedwwwwwwwwwwwww");
 		
 		System.out.println("ss");
 	}
